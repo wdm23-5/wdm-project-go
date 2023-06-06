0aa3449ae036da1f2ded39206b28494c3f73d7a7
dear-dd <57469918+dear-dd@users.noreply.github.com>
2023-06-07 00:14:36 +0200

    fix command

--- gke-scale/deploy.sh
+++ gke-scale/deploy.sh
@@ -1,19 +1,19 @@
 helm install nginx-ingress ingress-nginx/ingress-nginx
 
-helm install redis-order-1 bitnami/redis -f ${prefix}gke/helm/redis-helm-values.yaml
-helm install redis-order-2 bitnami/redis -f ${prefix}gke/helm/redis-helm-values.yaml
-helm install redis-order-3 bitnami/redis -f ${prefix}gke/helm/redis-helm-values.yaml
+helm install redis-order-1 bitnami/redis -f ${prefix}gke-scale/helm/redis-helm-values.yaml
+helm install redis-order-2 bitnami/redis -f ${prefix}gke-scale/helm/redis-helm-values.yaml
+helm install redis-order-3 bitnami/redis -f ${prefix}gke-scale/helm/redis-helm-values.yaml
 
-helm install redis-stock-1 bitnami/redis -f ${prefix}gke/helm/redis-helm-values.yaml
+helm install redis-stock-1 bitnami/redis -f ${prefix}gke-scale/helm/redis-helm-values.yaml
 
-helm install redis-payment-1 bitnami/redis -f ${prefix}gke/helm/redis-helm-values.yaml
+helm install redis-payment-1 bitnami/redis -f ${prefix}gke-scale/helm/redis-helm-values.yaml
 
-THIS_ID=1 envsubst < ${prefix}gke/kube/order-app.yaml.tpl | kubectl apply -f -
-THIS_ID=2 envsubst < ${prefix}gke/kube/order-app.yaml.tpl | kubectl apply -f -
-THIS_ID=3 envsubst < ${prefix}gke/kube/order-app.yaml.tpl | kubectl apply -f -
+curl -s ${prefix}gke-scale/kube/order-app.yaml.tpl | THIS_ID=1 envsubst | kubectl apply -f -
+curl -s ${prefix}gke-scale/kube/order-app.yaml.tpl | THIS_ID=2 envsubst | kubectl apply -f -
+curl -s ${prefix}gke-scale/kube/order-app.yaml.tpl | THIS_ID=3 envsubst | kubectl apply -f -
 
-THIS_ID=1 envsubst < ${prefix}gke/kube/stock-app.yaml.tpl | kubectl apply -f -
+curl -s ${prefix}gke-scale/kube/stock-app.yaml.tpl | THIS_ID=1 envsubst | kubectl apply -f -
 
-THIS_ID=1 envsubst < ${prefix}gke/kube/payment-app.yaml.tpl | kubectl apply -f -
+curl -s ${prefix}gke-scale/kube/payment-app.yaml.tpl | THIS_ID=1 envsubst | kubectl apply -f -
 
-kubectl apply -f ${prefix}gke/kube/ingress-service.yaml
+kubectl apply -f ${prefix}gke-scale/kube/ingress-service.yaml
